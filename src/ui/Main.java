7c540287185a29daa41042407ee62b03a6eb84f1
Mendo6472 <mendo6472@gmail.com>
2022-11-01 14:13:46 -0500

    Create Main
    Archivo para expandir luego

---
+++ src/ui/Main.java
@@ -0,0 +1,87 @@
+package ui;
+
+import java.util.Scanner;
+
+public class Main{
+
+    private Scanner reader;
+
+	public Main() {
+		reader = new Scanner(System.in);
+    }
+
+    public static void main(String[] args) {
+		Main main = new Main(); 
+		int option = 0; 
+		do{
+			option = main.getOptionShowMenu(); 
+			main.executeOption(option);
+		}while(option != 0);
+		main.getReader().close();
+	}
+
+    public int getOptionShowMenu(){
+		int option = 0; 
+		System.out.println("<<<<< Welcome to NeoTunes >>>>>");
+		System.out.println("0. Exit. ");
+		option =  validateIntegerInput();
+		return option; 
+	}
+
+    public void executeOption(int option){
+		switch(option){
+			case 1: 
+				break; 
+
+			case 2: 
+				break; 
+
+			case 3: 
+				break; 
+
+			case 0: 
+				System.out.println("Exit program.");
+				break; 
+
+			default: 
+				System.out.println("Invalid Option");
+				break; 
+		}
+	}
+
+    public int validateIntegerInput(){
+		int option = 0; 
+
+		if(reader.hasNextInt()){
+			option = reader.nextInt(); 
+			reader.nextLine();
+		}
+		else{
+			// clear reader. 
+			reader.nextLine(); 
+			option = -1; 
+		}
+
+		return option; 
+	}
+
+    public double validateDoubleInput(){
+		double option = 0; 
+
+		if(reader.hasNextDouble()){
+			option = reader.nextDouble(); 
+		}
+		else{
+			// clear reader. 
+			reader.nextLine(); 
+			option = -1; 
+		}
+
+		return option; 
+	}
+
+    public Scanner getReader(){
+		return reader; 
+	}
+
+}
